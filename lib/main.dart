565cdc63a6c3f1355a8188929fe85bf6e434621e
Abdellah <abdallahkaba98@gmail.com>
2025-03-20 11:16:10 +0000

    Ajout d'un champ de saisie pour afficher le nom en temps réel

--- lib/main.dart
+++ lib/main.dart
@@ -9,45 +9,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: CounterScreen());
+    return MaterialApp(home: NameInputScreen());
   }
 }
 
-// Definition d'un StatefulWidget
-
-class CounterScreen extends StatefulWidget {
-  const CounterScreen({super.key});
+class NameInputScreen extends StatefulWidget {
+  const NameInputScreen({super.key});
 
   @override
-  State<CounterScreen> createState() => _CounterScreenState();
+  State<NameInputScreen> createState() => _NameInputScreenState();
 }
 
-class _CounterScreenState extends State<CounterScreen> {
-  int _counter = 0;
-  void _incrementCounter() {
+class _NameInputScreenState extends State<NameInputScreen> {
+  String _name = "";
+  void _updateName(String value) {
     setState(() {
-      _counter++;
+      _name = value;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('StatefulWidget - Computer')),
-      body: Center(
+      appBar: AppBar(title: Text('Saisie du text')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              _counter > 5
-                  ? '🔥 Wow, tu as cliqué plus de 5 fois !'
-                  : 'Compteur: $_counter',
+              _name.isEmpty ? "Entrez votre nom" : "Bonjour, $_name !",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: Text('Incrément'),
+            TextField(
+              onChanged: _updateName,
+              decoration: InputDecoration(
+                labelText: "Votre nom",
+                border: OutlineInputBorder(),
+              ),
             ),
           ],
         ),
