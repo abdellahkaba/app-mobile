cd3099ef280d7294c409129a766580331c43dde1
Abdellah <abdallahkaba98@gmail.com>
2025-03-21 06:53:47 +0000

    Ajout de la navigation entre HomeScreen et DetailScreen

--- lib/main.dart
+++ lib/main.dart
@@ -9,85 +9,61 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: WidgetPracticeScreen(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
 
-class WidgetPracticeScreen extends StatefulWidget {
-  const WidgetPracticeScreen({super.key});
-
-  @override
-  State<WidgetPracticeScreen> createState() => _WidgetPracticeScreenState();
-}
-
-class _WidgetPracticeScreenState extends State<WidgetPracticeScreen> {
-  int _counter = 0;
-  Color _iconColor1 = Colors.blue;
-  Color _iconColor2 = Colors.red;
-  Color _iconColor3 = Colors.green;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _toggleIconColor(int iconIndex) {
-    setState(() {
-      if (iconIndex == 1) {
-        _iconColor1 = _iconColor1 == Colors.blue ? Colors.orange : Colors.blue;
-      } else if (iconIndex == 2) {
-        _iconColor2 = _iconColor2 == Colors.red ? Colors.purple : Colors.red;
-      } else if (iconIndex == 3) {
-        _iconColor3 = _iconColor3 == Colors.green ? Colors.black : Colors.green;
-      }
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Exercice Dynamique')),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
+      appBar: AppBar(title: Text('Acceuil')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder:
+                    (context) => DetailScreen(
+                      message: "Bonjour depuis l'ecran principal",
+                    ),
+              ),
+            );
+          },
+          child: Text("Aller a l'ecran detail"),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final String message;
+
+  const DetailScreen({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Écran Détails')),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Le titre centré
-            Center(
-              child: Text(
-                "Compteur: $_counter",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
+            Text(
+              message, // Affiche le message reçu
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
             ),
-            SizedBox(height: 30),
-            // Button a incrementer
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _incrementCounter,
-              child: Text('Incrémenter'),
-            ),
-            SizedBox(height: 50),
-
-            //Trois icone interratives en Row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  onPressed: () => _toggleIconColor(1),
-                  icon: Icon(Icons.home, size: 40, color: _iconColor1),
-                ),
-                IconButton(
-                  onPressed: () => _toggleIconColor(2),
-                  icon: Icon(Icons.favorite, size: 40, color: _iconColor2),
-                ),
-                IconButton(
-                  onPressed: () => _toggleIconColor(3),
-                  icon: Icon(Icons.settings, size: 40, color: _iconColor3),
-                ),
-              ],
+              onPressed: () {
+                Navigator.pop(context); // Retour à l'écran précédent
+              },
+              child: Text("Retour à l'Accueil"),
             ),
           ],
         ),
